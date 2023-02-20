8f02bac5a0f9b7c65b6a0362cb585bdb7b3a4218
Hs-123456 <rameshkumarsaini275@gmail.com>
2023-02-20 06:57:47 +0530

    update

---
+++ constant/constant.js
@@ -0,0 +1,9 @@
+module.exports = {
+    message: "success",
+    internalServerError: "Internal server error",
+    noBucketFound: "Bucket not found",
+    authFailed: "Authentication failed",
+    messageBall: "You can use these ball in",
+    ballUsing: "ball using in",
+    size: "size less than zero"
+}

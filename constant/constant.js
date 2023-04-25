ba01a49f53bbb917afc1f09f62d14bef337999f3
sainiRJ <rajeshpushpakar01@gmail.com>
2023-04-25 15:39:53 +0530

    Initial commit

--- constant/constant.js
+++ constant/constant.js
@@ -3,7 +3,7 @@ module.exports = {
     internalServerError: "Internal server error",
     noBucketFound: "Bucket not found",
     authFailed: "Authentication failed",
-    messageBall: "You can use these ball in",
+    messageBall: "You can add these ball in",
     ballUsing: "ball using in",
     size: "size less than zero",
     existEmail: "Email already exist"
--- dao/createVolume.js
+++ dao/createVolume.js
@@ -1,10 +1,14 @@
 const volumeBalls = require('../model/volumeBalls')
 const volumeBuckets = require('../model/volumeBuckets')
 
-exports.createVolumeBall = async function(volumeData){
-    return await volumeBalls.create(volumeData)
+exports.createVolumeBall = async function(data){
+    const filter = {userId: data.userId}
+    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+    return await volumeBalls.findOneAndUpdate(filter,data,options)
 }
 
-exports.createVolumeBucket = async function(volumeData){
-    return await volumeBuckets.create(volumeData)
+exports.createVolumeBucket = async function(data){
+    const filter = {userId: data.userId}
+    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+    return await volumeBuckets.findOneAndUpdate(filter,data,options)
 }
